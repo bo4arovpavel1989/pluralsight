39876f86e503fc021abdc63ecbd5ea931c8bc743
User <bo4arovpavel1989@mail.ru>
2019-07-29 16:12:40 +0300

    made basic redux flow

--- Redux/src/components/courses/CoursesPage.js
+++ Redux/src/components/courses/CoursesPage.js
@@ -1,9 +1,74 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursePage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  /* Стрелочная функция, чтобы не биндить функцию в конструкторе */
+  handleChange = event => {
+    /* Клонируем state, чтобы сохранять его неизменным */
+    const course = { ...this.state.course, title: event.target.value };
+
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.actions.createCourse(this.state.course);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3> Add course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
+    );
   }
 }
 
-export default CoursePage;
+CoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+// функция связывает данные в store и props в компоненте
+function mapStateToProps(state) {
+  // Будьте точны. Запрашивайте только данные, которые нужны компоненту.
+  // Компонент перерендерится когда изменятся данные в store
+  return {
+    courses: state.courses
+  };
+}
+
+// функция оборачивае все вызовы action в dispatch
+// и возвращает один объект actions
+function mapDispatchToProps(dispatch) {
+  // обязательно вызывайте dispatch, иначе ничего не произойдет
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
+}
+
+// connect автоматически добавляет dispatch в props,
+// если мы опускаем mapDispatchToProps (2-й аргумент connect)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CoursePage);
--- Redux/src/index.js
+++ Redux/src/index.js
@@ -4,10 +4,18 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+// стоит исользовать initial state в store здесь, если это серверный рендеринг или
+// идет инициализация из localStorage
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
