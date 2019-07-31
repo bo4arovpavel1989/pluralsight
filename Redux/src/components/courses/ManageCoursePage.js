d09992f8d885a828602ea62ec8bf331ea62c488d
User <bo4arovpavel1989@mail.ru>
2019-07-31 11:59:45 +0300

    added creating and updating state

---
+++ Redux/src/components/courses/ManageCoursePage.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import * as authorActions from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  saveCourse,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("Loading courses failed " + error);
+      });
+    } else {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("Loading authors failed " + error);
+      });
+    }
+  }, [props.course]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setCourse(prevCourse => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    });
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired
+};
+
+export function getCourseBySlug(courses, slug) {
+  return courses.find(course => course.slug === slug) || null;
+}
+
+// функция связывает данные в store и props в компоненте
+function mapStateToProps(state, ownProps) {
+  // Будьте точны. Запрашивайте только данные, которые нужны компоненту.
+  // Компонент перерендерится когда изменятся данные в store
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+
+  return {
+    course,
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+// object form of mapDispatchToProps
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors,
+  saveCourse: courseActions.saveCourse
+};
+
+// connect автоматически добавляет dispatch в props,
+// если мы опускаем mapDispatchToProps (2-й аргумент connect)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
